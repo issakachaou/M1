00613f7c1f056e2cf410435941ad4c1ae7f98ccc
Issa Kachaou <issakachaou@outlook.fr>
2024-09-14 21:53:12 +0200

    14.09

---
+++ Excel - VBA/COURS_01.vb
@@ -0,0 +1,27 @@
+'Cours no. 1 : 14 septembre
+
+'Transformation de chiffre en caractère sous condition
+
+SI(ALEA.ENTRE.BORNES(1;2)=1;"M";"F")
+
+'Fonction de SI impriqué
+
+SI(ALEA.ENTRE.BORNES(1;3)=1;"A";SI(ALEA.ENTRE.BORNES(1;3)=2;"B";"C"))
+
+'Alternative plus simple au SI impriqué
+
+=CHOISIR(ALEA.ENTRE.BORNES(1;3);"A";"B";"C")
+
+'Fonction rechecheV "V lookup"
+
+=RECHERCHEV(D2;NIVEAU!$A$2:$C$4;2;FAUX)
+
+'faire un indentifiant avec la fonction CONCATENER
+
+=CONCATENER(B2;D2;E2)
+
+'faire un indentifiant de manière plus simple 
+
+=B2&D2&E
+
+=STXT(A2;NBCAR(A2);1)
